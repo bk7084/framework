57b45f7b612c2c173c82f1ef75119f80ed847eae
matthiascy <matthiasychen@gmail.com>
2021-12-08 22:38:04 +0100

    add another wall

--- examples/construction.py
+++ examples/construction.py
@@ -42,10 +42,13 @@ class Wall(Component):
 wall = Wall(1.0, 1.0, texture1='textures/checker_small.png', texture2='textures/checker_color.png')
 wall2 = Wall(1.0, 1.0, texture1='textures/checker_huge.png', texture2='textures/checker_large.png')
 wall2.transform = Mat4.from_translation(Vec3(1.0, 0.0, 0.0))
+wall3 = Wall(1.0, 1.0, texture1='textures/checker_small.png', texture2='textures/checker_color.png')
+wall3.transform = Mat4.from_rotation_y(90, True) * Mat4.from_translation(Vec3(1.4, 0.0, 0.0))
 
 building = Building()
 building.append(wall)
 building.append(wall2, wall)
+building.append(wall3, wall)
 
 
 @window.event
