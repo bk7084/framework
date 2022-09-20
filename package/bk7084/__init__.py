26ab2e79c9bef3cc705fb12fa2770ecbc8fd302f
matthiascy <matthiasychen@gmail.com>
2022-09-20 15:55:56 +0200

    refactor: introduce rust toolchain.

--- package/bk7084/__init__.py
+++ package/bk7084/__init__.py
@@ -1,4 +1,5 @@
 """Framework written in Python for BK7084 Computational Simulations"""
+from .bk7084 import *
 
 from . import app
 from . import camera
@@ -8,4 +9,10 @@ from .app.window import *
 from .app import ui
 from . import math
 
-__version__ = (0, 1, 9)
+__version__ = (0, 2, 0)
+
+
+__doc__ = bk7084.__doc__
+
+if hasattr(bk7084, "__all__"):
+	__all__ = bk7084.__all__
---
+++ package/examples/000_window.py
@@ -0,0 +1,4 @@
+import bk7084
+
+if __name__ == "__main__":
+    bk7084.run()
